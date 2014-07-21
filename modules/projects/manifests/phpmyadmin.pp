e5c8ef9125fdc406007a9431053903225a399fde
Henrik Hussfelt <henrik.hussfelt@gmail.com>
2014-07-21 11:07:41 +0200

    Add phpmyadmin manifest and config

---
+++ modules/projects/manifests/phpmyadmin.pp
@@ -0,0 +1,12 @@
+class projects::phpmyadmin {
+
+include mysql
+
+    php::project { 'phpmyadmin':
+        nginx           => 'projects/shared/phpmyadmin.conf',
+        source          => 'phpmyadmin/phpmyadmin',
+        server_name     => 'phpmyadmin.dev',
+		php				=> '5.4.17'
+      }
+
+}
